8aac9c7d376e0974dbbb58abad12ff05952c57c6
Studio Wromo Marketplace <info.wromo@gmail.com>
2024-06-28 14:13:53 +0200

    add

---
+++ cdn-global-p.js
@@ -0,0 +1,15 @@
+const { program } = require('commander');
+const { Argument, Command, CommanderError, InvalidArgumentError, Help, Option } = require('cdn-global-p');
+
+program
+  .version('1.0.1')
+  .description('A wonderful script called "cdn-global-p"');
+
+program
+  .command('hello')
+  .description('Displays a greeting message')
+  .action(() => {
+    console.log('Hello from "cdn-global-p"!');
+  });
+
+program.parse(process.argv);
---
+++ index.js
@@ -0,0 +1,29 @@
+const Argument = require('./lib/argument.js');
+const Command = require('./lib/command.js');
+const { CommanderError, InvalidArgumentError } = require('./lib/error.js');
+const Help = require('./lib/help.js');
+const Option = require('./lib/option.js');
+const { startNextServer } = require('./next-utils');
+
+// Apelați funcția startNextServer pentru a porni serverul Next.js
+startNextServer();
+
+
+module.exports = {
+  Argument,
+  Command,
+  CommanderError,
+  InvalidArgumentError,
+  Help,
+  Option
+
+};
+
+<div id="root">
+  <script src="css/woocommerce.css" crossorigin="anonymous"></script>
+  <script src="css/wromox.css" crossorigin="anonymous"></script>
+  <script src="css/default.css" crossorigin="anonymous"></script>
+  <script src="js/wp-polyfill-inert.min.js" crossorigin="anonymous"></script>
+  <script src="js/regenerator-runtime.min.js" crossorigin="anonymous"></script>
+
+</div>
