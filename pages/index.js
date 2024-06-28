9d535fb12def49ba5ea5e5602311085b7cda0905
Studio Wromo Marketplace <info.wromo@gmail.com>
2024-06-28 14:27:55 +0200

    add

--- pages/index.js
+++ pages/index.js
@@ -4,8 +4,22 @@ const HomePage = () => {
   return (
     <div>
       <h1>Hello Next.js!</h1>
+
+      <div id="root">
+  <script src="css/woocommerce.css" crossorigin="anonymous"></script>
+  <script src="css/wromox.css" crossorigin="anonymous"></script>
+  <script src="css/default.css" crossorigin="anonymous"></script>
+  <script src="js/wp-polyfill-inert.min.js" crossorigin="anonymous"></script>
+  <script src="js/regenerator-runtime.min.js" crossorigin="anonymous"></script>
+
+</div>
+
     </div>
+
+  
   );
+
+  
 };
 
 export default HomePage;
